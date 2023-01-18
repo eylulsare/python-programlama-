1fd6c5f877d2fdebbcc80c7c1b4371eca98ff712
eylulsare <77004063+eylulsare@users.noreply.github.com>
2023-01-18 21:17:55 +0300

    Python alıştırmaları

---
+++ 1. Ders/3_degiskenler.py
@@ -0,0 +1,23 @@
+a = 3
+b = None
+c = 0.5
+d = -1234567
+print(d)
+e = True
+print(e)
+print(a, b, c, d, e)
+print(a, b, c, d, e, sep=", ")
+
+print(a, b, end="")
+print(" aynı satırdayım")
+
+print(a); print(b); print(c)
+
+a, b, c = 3, 5, 7
+print(a, b, c)
+
+t = "asdf"
+f = 'qewr'
+g = 'a'
+
+print(t, f, g)
